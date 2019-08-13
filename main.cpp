2813f903fb2d5e90601c9de8d222e59618df47fd
lpc1996 <lpc1235@163.com>
2019-08-13 20:23:07 +0800

    文件读取

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include "main.h"
 #include "File_IO.h"
+#include "BookModel.h"
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
 	File_IO fio;
 	vector<BookModel> vec;
 	vec = fio.ReadFile();
-	cout << vec.size() <<endl;
+	cout << "vec.size()=" <<vec.size() <<endl;
 	return 0;
 }
--- main.h
+++ main.h
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "BookModel.h"
 
 using namespace std;
 
